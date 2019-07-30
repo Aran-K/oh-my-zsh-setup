7bc8e1b6e2217d9a1dc9343b90b45cfad008c5ac
Aran K <aran361@gmail.com>
2019-07-30 12:31:40 +0200

    Fixing double insert bug and new syntax hightlighter

--- load_custom_plugins.sh
+++ load_custom_plugins.sh
@@ -1,14 +1,14 @@
 CUSTOM_MSG="#This line was inserted to load the list of custom plugins as spacified in 'load_custom_plugins'"
-CUSTOM_PLUGIN_LIST="git colored-man-pages zsh-autosuggestions zsh-syntax-highlighting"
+CUSTOM_PLUGIN_LIST="git colored-man-pages zsh-autosuggestions fast-syntax-highlighting"
 
 
 
 if grep -q "$CUSTOM_MSG" ~/.zshrc; then
     echo "Updating .zshrc"
-    sed -i "s/^plugins=/plugins=($CUSTOM_PLUGIN_LIST)\n/" ~/.zshrc
+    sed -i "s/^plugins=.*/plugins=($CUSTOM_PLUGIN_LIST)/" ~/.zshrc
 else
     echo "No config found. Adding new config to .zshrc"  
-    sed -i "s/^plugins=/$CUSTOM_MSG\nplugins=($CUSTOM_PLUGIN_LIST)\n/" ~/.zshrc
+    sed -i "s/^plugins=.*/$CUSTOM_MSG\nplugins=($CUSTOM_PLUGIN_LIST)/" ~/.zshrc
 fi
 
 
