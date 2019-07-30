5b2a717499081625319fdaf28ffa4ae630bd5847
Aran K <aran361@gmail.com>
2019-07-30 12:20:12 +0200

    Fixing dirty hack. Ish

--- load_custom_plugins.sh
+++ load_custom_plugins.sh
@@ -1,4 +1,14 @@
-CUSTOM_MSG="#This line was inserted to load the list of custom plugins as spacified in 'load_custom_plugins'\n#It may appear more than once, all but the newest are safe to delete"
+CUSTOM_MSG="#This line was inserted to load the list of custom plugins as spacified in 'load_custom_plugins'"
 CUSTOM_PLUGIN_LIST="git colored-man-pages zsh-autosuggestions zsh-syntax-highlighting"
 
-sed -i  "1s/^/$CUSTOM_MSG\nplugins=($CUSTOM_PLUGIN_LIST)\n\n/" ~/.zshrc
+
+
+if grep -q "$CUSTOM_MSG" ~/.zshrc; then
+    echo "Updating .zshrc"
+    sed -i "s/^plugins=/plugins=($CUSTOM_PLUGIN_LIST)\n/" ~/.zshrc
+else
+    echo "No config found. Adding new config to .zshrc"  
+    sed -i "s/^plugins=/$CUSTOM_MSG\nplugins=($CUSTOM_PLUGIN_LIST)\n/" ~/.zshrc
+fi
+
+
